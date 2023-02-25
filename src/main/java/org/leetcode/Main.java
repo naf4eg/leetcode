b3625171a4690e9096882849d016e93e52917941
Konstantin <naf4eg@yandex.ru>
2023-02-26 01:43:43 +0300

    valid anagram 3 solutions

--- src/main/java/org/leetcode/Main.java
+++ src/main/java/org/leetcode/Main.java
@@ -3,6 +3,8 @@ package org.leetcode;
 
 public class Main {
     public static void main(String[] args) {
-
+        int[] arr = {1,2};
+        arr[0]++;
+        System.out.println(arr);
     }
 }
--- src/main/java/org/leetcode/valid_anagram/Solution.java
+++ src/main/java/org/leetcode/valid_anagram/Solution.java
@@ -6,6 +6,18 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println(isAnagram("anagram", "nagaram"));
         System.out.println(isAnagram("rat", "car"));
+
+        System.out.println(isAnagram2("anagram", "nagaram"));
+        System.out.println(isAnagram2("rat", "car"));
+
+        System.out.println(isAnagram3("anagram", "nagaram"));
+        System.out.println(isAnagram3("rat", "car"));
+
+
+
+        System.out.println("ana".charAt(1) - 'a');
+        System.out.println(Character.getNumericValue('a'));
+        System.out.println(Character.getNumericValue('n'));
     }
 
     static boolean isAnagram(String s, String t) {
@@ -18,5 +30,39 @@ public class Solution {
         Arrays.sort(tArr);
 
         return Arrays.equals(sArr, tArr);
+    }
+
+    static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] array = new int[26]; //26 символов в алфавите
+        for(int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a'; //порядковый номер буквы относительно а
+            array[index]++; //инкрементируем сколько раз встретилась буква в слове
+        }
+        for (int i = 0; i < t.length(); i++) {
+            int index = t.charAt(i) - 'a';
+            array[index]--;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) return false;
+        }
+
+        return true;
+    }
+
+    static boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] array = new int[26]; //26 символов в алфавите
+        for(int i = 0; i < s.length(); i++) {
+            array[s.charAt(i) - 'a']++; //инкрементируем сколько раз встретилась буква в слове
+            array[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) return false;
+        }
+
+        return true;
     }
 }
